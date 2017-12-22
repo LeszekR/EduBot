1e3a844a010a7c7e86063506cbb947f04788c67b
mikołaj <mikolaj.klein@gmail.com>
2017-12-22 23:27:47 +0100

    style quiz

--- EduBot/EduClient/src/app/views/module-view/quiz-view/quiz-view.component.ts
+++ EduBot/EduClient/src/app/views/module-view/quiz-view/quiz-view.component.ts
@@ -6,7 +6,36 @@ import { ClosedQuestion } from '../../../models/closed-question';
 // ==================================================================================================================
 @Component({
   selector: 'quiz-view',
-  templateUrl: './quiz-view.component.html'
+  templateUrl: './quiz-view.component.html',
+  styles: [
+    `
+        fieldset {
+          border: 1px solid #ddd !important;
+          margin: 0;
+          min-width: 0;
+          padding: 10px;       
+          position: relative;
+          border-radius:4px;
+          background-color:#B5CCF6 !important;
+          padding-left:10px!important;
+      }	
+      
+      legend {
+          font-size:14px;
+          font-weight:bold;
+          margin-bottom: 0px; 
+          width: 35%; 
+          border: 1px solid #ddd;
+          border-radius: 4px; 
+          padding: 5px 5px 5px 10px; 
+          background-color: #ffffff;
+      }
+
+      .bg-card{
+          background-color:#B5CCF6;
+      }
+    `
+  ]
 })
 export class QuizViewComponent {
 

01e88ea8a46f81783db28fc775f24dff9ea4ba98
mikołaj <mikolaj.klein@gmail.com>
2017-12-23 18:40:45 +0100

    Edycja pytań i odpowiedzi działa

--- EduBot/EduClient/src/app/views/module-view/quiz-view/question-view/question-view.component.ts
+++ EduBot/EduClient/src/app/views/module-view/quiz-view/question-view/question-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { ClosedQuestion } from '../../../../models/closed-question'
 
@@ -12,7 +12,10 @@ export class QuestionViewComponent implements OnInit {
 
   @Input() questionData: ClosedQuestion;
   @Input() editMode: boolean;
+  @Output() onUpdateQuestion = new EventEmitter<ClosedQuestion>();
 
+  editQuestionText: boolean;
+  editedAnswerIdx: number;
 
   // CONSTRUCTOR
   // ==============================================================================================================
@@ -29,6 +32,26 @@ export class QuestionViewComponent implements OnInit {
   // PRIVATE
   // ==============================================================================================================
 
+  private addAnswerToQuestion(){
+    this.questionData.answers.push("Nowa odpowiedź");
+    this.updateSource();
+  }
+
+  private updateQuestionValue(value: string){
+    this.questionData.question = value;
+    this.editQuestionText = false;
+    this.updateSource();
+  }
+
+  private updateAnswerValue(value: string){
+    this.questionData.answers[this.editedAnswerIdx] = value;
+    this.editedAnswerIdx = null;
+    this.updateSource();
+  }
+
+  private updateSource(){
+    this.onUpdateQuestion.emit(this.questionData);
+  }
 }
 
 
--- EduBot/EduClient/src/app/views/module-view/quiz-view/quiz-view.component.ts
+++ EduBot/EduClient/src/app/views/module-view/quiz-view/quiz-view.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 import { ClosedQuestion } from '../../../models/closed-question';
-import { ContextService } from '../../../services/context.service';
 
 
 // ==================================================================================================================
@@ -15,7 +14,7 @@ export class QuizViewComponent {
   @Input() questions: ClosedQuestion[];
   @Input() readonly: boolean;
 
-  constructor(private context: ContextService){}
+  constructor(){}
 
   
   // CONSTRUCTOR
@@ -29,10 +28,14 @@ export class QuizViewComponent {
   // PRIVATE
   // ==============================================================================================================
   private addQuestion(){
-
+    let question = new ClosedQuestion();
+    question.question = "Nowe pytnie";
+    this.questions.push(question);
   }
 
-  private addAnswer(){
-
+  private updateQuestion(question: ClosedQuestion, idx: number){
+    this.questions[idx] = question;
   }
+
+
 }

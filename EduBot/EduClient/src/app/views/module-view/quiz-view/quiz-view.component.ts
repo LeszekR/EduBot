52aafeffcd29d34d2dcc9adc58fa0c9382960149
mikołaj <mikolaj.klein@gmail.com>
2017-12-23 11:43:50 +0100

    Przygotowanie do trybu edycji pytania

--- EduBot/EduClient/src/app/views/module-view/quiz-view/quiz-view.component.ts
+++ EduBot/EduClient/src/app/views/module-view/quiz-view/quiz-view.component.ts
@@ -1,46 +1,21 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 import { ClosedQuestion } from '../../../models/closed-question';
+import { ContextService } from '../../../services/context.service';
 
 
 // ==================================================================================================================
 @Component({
   selector: 'quiz-view',
   templateUrl: './quiz-view.component.html',
-  styles: [
-    `
-        fieldset {
-          border: 1px solid #ddd !important;
-          margin: 0;
-          min-width: 0;
-          padding: 10px;       
-          position: relative;
-          border-radius:4px;
-          background-color:#B5CCF6 !important;
-          padding-left:10px!important;
-      }	
-      
-      legend {
-          font-size:14px;
-          font-weight:bold;
-          margin-bottom: 0px; 
-          width: 35%; 
-          border: 1px solid #ddd;
-          border-radius: 4px; 
-          padding: 5px 5px 5px 10px; 
-          background-color: #ffffff;
-      }
-
-      .bg-card{
-          background-color:#B5CCF6;
-      }
-    `
-  ]
+  styleUrls: ['quiz-view.component.css']
 })
 export class QuizViewComponent {
 
   @Input() questions: ClosedQuestion[];
   @Input() readonly: boolean;
+
+  constructor(private context: ContextService){}
 
   
   // CONSTRUCTOR
@@ -53,4 +28,11 @@ export class QuizViewComponent {
 
   // PRIVATE
   // ==============================================================================================================
+  private addQuestion(){
+
+  }
+
+  private addAnswer(){
+
+  }
 }

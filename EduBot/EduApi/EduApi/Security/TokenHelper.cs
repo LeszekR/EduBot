73e71c74b6fed4cef8cba8c878864370410e1599
Mikolaj <mikolaj.klein@gmail.com>
2017-12-29 07:23:59 +0100

    TokenHelper - pobieranie danych z tokena

---
+++ EduBot/EduApi/EduApi/Security/TokenHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace EduApi.Security
+{
+    public static class TokenHelper
+    {
+        public static int? GetUserId(IIdentity identity)
+        {
+            return getClaim((ClaimsIdentity)identity, "userId");
+        }
+
+        private static int? getClaim(ClaimsIdentity identity, string claimType)
+        {
+            ClaimsIdentity claimsIdentity = (ClaimsIdentity)identity;
+            IEnumerable<Claim> claims = claimsIdentity.Claims;
+            var claim = claims.Where(x => x.Type == claimType).FirstOrDefault();
+
+            return claim != null ? int.Parse(claim.Value) : (int?)null;
+        }
+    }
+}

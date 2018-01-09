c4fc6a2da2aed84ce841ba6a3f4a527408adf5b0
Leszek Rudnicki <zarzur@wp.pl>
2018-01-09 16:42:36 +0100

    Nowy CodeTask i wykonanie kodu HTML

---
+++ EduBot/EduClient/src/app/models/enums.ts
@@ -0,0 +1,39 @@
+// ==================================================================================================================
+export enum DiffLevel {
+    Easy = 0,
+    Medium = 1,
+    Hard = 2
+}
+
+// ==================================================================================================================
+export enum Role {
+    admin = 1,
+    teacher = 2,
+    student = 3
+}
+
+// ==================================================================================================================
+export enum TestResult {
+    None,
+    Correct,
+    Incorrect
+}
+
+// ==================================================================================================================
+export class CodeMode {
+    static JAVASCRIPT = "javascript";
+    static HTML = "html";
+}
+// ------------------------------------------------------------------------------------------------------------------
+export class CodeModeMapper {
+
+    public static makeMode(mode: string): CodeMode {
+        switch (mode) {
+            case "javascript":
+                return CodeMode.JAVASCRIPT;
+            case "javascript":
+                return CodeMode.HTML;
+        }
+        return null;
+    }
+}

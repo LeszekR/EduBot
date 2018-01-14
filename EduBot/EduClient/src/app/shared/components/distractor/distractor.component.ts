7c11ab972c571fc0aecf5fefbcf1cf1f85e9e01e
mikołaj <mikolaj.klein@gmail.com>
2018-01-14 16:13:31 +0100

    distractor-component

---
+++ EduBot/EduClient/src/app/shared/components/distractor/distractor.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnDestroy } from '@angular/core';
+import { DistractorService, DistractorType } from '../../../services/distractor.service';
+
+@Component({
+    moduleId: module.id,
+    selector: 'distractor-component',
+    templateUrl: `./distractor.component.html`
+})
+export class DistractorComponent implements OnDestroy {
+
+    private KEY_ESC = 27;
+    private IMG_PATH = '/assets/img/';
+
+    private showDistractor: boolean;
+    private distractorSubsciption: any;
+    private imgSrc;
+
+    constructor(private service: DistractorService) {
+        this.distractorSubsciption = this.service.onShowDistrctor.subscribe(d => this.show(d));
+    }
+
+    ngOnDestroy(){
+        this.distractorSubsciption.unsubscribe();
+    }
+
+    private show(type: DistractorType){
+        this.imgSrc = this.getImgSrc(type);
+        this.showDistractor = true;
+
+        document.onkeydown = (e:any) => {
+            if(e.which == this.KEY_ESC){
+                this.hide();
+            } 
+        }
+    }
+
+    private hide(){
+        this.imgSrc = null;
+        this.showDistractor = false;
+    }
+
+    private getImgSrc(type: DistractorType): string{
+        switch(type){
+            case DistractorType.SmallExplosion:
+                return this.IMG_PATH + "logo-pg.png";
+            case DistractorType.MediumExplosion:
+                return this.IMG_PATH + "logo-pg.png";
+            case DistractorType.BigExplosion:
+                return this.IMG_PATH + "logo-pg.png";
+            case DistractorType.DisarmedMine:
+                return this.IMG_PATH + "logo-pg.png";
+            case DistractorType.HiddenMine:
+                return this.IMG_PATH + "logo-pg.png";
+            case DistractorType.Promotion:
+                return this.IMG_PATH + "logo-pg.png";
+            default: 
+                return null;
+        }
+    }
+
+}

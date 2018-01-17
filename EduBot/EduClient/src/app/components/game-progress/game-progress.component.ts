401f9a204401bd806f25a6e3c6c9677ca8489b79
Leszek Rudnicki <zarzur@wp.pl>
2018-01-17 03:54:54 +0100

    Paski życia i ochrony

--- EduBot/EduClient/src/app/components/game-progress/game-progress.component.ts
+++ EduBot/EduClient/src/app/components/game-progress/game-progress.component.ts
@@ -13,30 +13,62 @@ import { ContextService } from '../../services/context.service';
 export class GameProgressComponent implements OnInit {
 
   @Input() gameScore: GameScore;
-  
-  private rank: MilitaryRank;
-  private life: number;
-  private armour: number;
 
-  private numbers: number[];
+  private rank: MilitaryRank;
+  // private life: number;
+  // private armour: number;
+  // private numbers: number[];
 
   private score = this.context.gameScore;
-  private lifeSects = 20;
-  private shieldSects = 5;
-  
+
+  private readonly nLife = 20;
+  private lifeSegments: number[] = [];
+
+  private readonly shieldMax = 50;
+  private readonly nShield = 5;
+  private shieldSegments: number[] = [this.nShield];
+
 
   // CONSTRUCTOR
   // ==============================================================================================================
-  constructor(private context: ContextService){}
+  constructor(private context: ContextService) { }
 
   // --------------------------------------------------------------------------------------------------------------
-  ngOnInit(){
-	this.numbers = new Array();
-	for(let i = 1; i <= 100; ++i)
-		this.numbers.push(i);
+  ngOnInit() {
+    // this.numbers = new Array();
+    // for (let i = 1; i <= 100; ++i)
+    //   this.numbers.push(i);
 
-	this.rank = MilitaryRank.Soldier;
-	this.life = 50;
-	this.armour = 100;
+    // mMOCK =======================
+    this.context.gameScore.life = 28;
+    this.context.gameScore.shield = 43;
+    // mMOCK =======================
+
+    this.lifeSegments = new Array();
+    for (let i = 1; i <= this.nLife; i++)
+      this.lifeSegments.push(i * 100 / this.nLife);
+
+    this.shieldSegments = new Array();
+    for (let i = 1; i <= this.nShield; i++)
+      this.shieldSegments.push(i * 50 / this.nShield);
+
+    this.rank = MilitaryRank.Soldier;
+  }
+
+
+  // PRIVATE
+  // ==============================================================================================================
+  private segmentWidth(scoreMax: number, nSegMax: number, scoreValue: number, segValue: number) {
+
+    let x = scoreMax / nSegMax;
+
+    if (segValue <= scoreValue)
+      return 100;
+
+    let diff = segValue - scoreValue;
+    if (diff < x)
+      return (x-diff) / x * 100;
+
+    return 0;
   }
 }	
